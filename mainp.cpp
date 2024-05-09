8014a697db813449f8553fa1894137d05fdc7bce
Danil2802 <danilkogumbaev@yandex.ru>
2024-05-09 13:07:41 +0700

    i

--- mainp.cpp
+++ mainp.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// Функция для вычисления периметра прямоугольника
+// Функция для вычисления периметра прямоугольника87878787
 float calculateRectanglePerimeter(float length, float width) {
     return 2 * (length + width);
 }
